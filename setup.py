d5ed19ae031735accf683fd7ec63dbd23591236c
kimhyunjin <khjzzm@gmail.com>
2018-02-28 15:13:10 +0900

    Python v3.6

--- setup.py
+++ setup.py
@@ -39,5 +39,6 @@ setup(name = "popbill",
                      "Programming Language :: Python :: 3",
                      "Programming Language :: Python :: 3.3",
                      "Programming Language :: Python :: 3.4",
-                     "Programming Language :: Python :: 3.5"]
+                     "Programming Language :: Python :: 3.5",
+                     "Programming Language :: Python :: 3.6"]
       )
