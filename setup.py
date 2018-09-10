e56e141f9588045012d3ab73b49101a2101aa7a0
kimhyunjin <khjzzm@gmail.com>
2018-09-10 14:46:07 +0900

    Python 3.7 Support

--- setup.py
+++ setup.py
@@ -40,5 +40,6 @@ setup(name = "popbill",
                      "Programming Language :: Python :: 3.3",
                      "Programming Language :: Python :: 3.4",
                      "Programming Language :: Python :: 3.5",
-                     "Programming Language :: Python :: 3.6"]
+                     "Programming Language :: Python :: 3.6",
+                     "Programming Language :: Python :: 3.7"]
       )
