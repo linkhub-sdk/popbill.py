9184d30748987c7f2dcc20ecabd7ebc9594220a9
JohnJeong123 <frenchofkiss@gmail.com>
2020-05-18 17:45:36 +0900

    version up

--- popbill/__init__.py
+++ popbill/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.14.0'
+__version__ = '1.15.0'
 Version = __version__  # for backward compatibility
 __all__ = ["PopbillException", "JoinForm", "ContactInfo", "CorpInfo",
            "TaxinvoiceService", "Taxinvoice", "TaxinvoiceDetail", "Contact",
