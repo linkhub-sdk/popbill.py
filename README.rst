355497092d61462451ac166b18c63901e6468f4e
JohnJeong123 <frenchofkiss@gmail.com>
2020-06-29 11:03:38 +0900

    fixed version typo

--- README.rst
+++ README.rst
@@ -1,7 +1,7 @@
 ####
 popbill.py
 ####
-popbill 1.18.0
+popbill 1.18.1
 
 팝빌 API for Python.
 
--- popbill/__init__.py
+++ popbill/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.17.0'
+__version__ = '1.18.1'
 Version = __version__  # for backward compatibility
 __all__ = ["PopbillException", "JoinForm", "ContactInfo", "CorpInfo",
            "TaxinvoiceService", "Taxinvoice", "TaxinvoiceDetail", "Contact",
