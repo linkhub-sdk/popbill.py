ea787dbc89364a1d89385d10234e1727756dccf2
1jae <wjkim@linkhub.co.kr>
2021-04-22 18:35:38 +0900

    fixed fax comment(Updated)

--- popbill/faxService.py
+++ popbill/faxService.py
@@ -7,7 +7,7 @@
 # Author : Kim Seongjun (pallet027@gmail.com)
 # Written : 2015-01-21
 # Contributor : Jeong Yohan (code@linkhub.co.kr)
-# Updated : 2018-08-09
+# Updated : 2021-04-22
 # Thanks for your interest.
 from datetime import datetime
 from .base import PopbillBase, PopbillException, File
