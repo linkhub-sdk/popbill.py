5aab2ac1e4be0556181228ebaad5dafbf1aba502
1jae <wjkim@linkhub.co.kr>
2021-04-27 18:39:51 +0900

    v1.21.1 version up linkhub module

--- popbill/__init__.py
+++ popbill/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.21.0'
+__version__ = '1.21.1'
 Version = __version__  # for backward compatibility
 __all__ = ["PopbillException", "JoinForm", "ContactInfo", "CorpInfo",
            "TaxinvoiceService", "Taxinvoice", "TaxinvoiceDetail", "Contact",
