13c9b41d3367b7c405a53d6c34913c2183517c6e
AlBan93 <albaneo0724@gmail.com>
2023-03-08 12:06:22 +0900

    base.py 테스트 케이스 작성

---
+++ baseTest.py
@@ -0,0 +1,94 @@
+import sys
+import unittest
+from popbill.base import PopbillBase, PaymentForm, RefundForm
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+
+class BaseTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.baseService = PopbillBase(
+            'TESTER', 'SwWxqU+0TErBXy/9TVjIPEnI0VTUMMSQZtJf3Ed8q3I=')
+        self.baseService.IsTest = True
+        self.testCorpNum = "1234567890"
+        self.testUserID = "testkorea"
+
+    def test_paymentRequest(self):
+
+        CorpNum = "1234567890"
+        paymentForm = PaymentForm(
+            settlerName="담당자명",
+            settlerEmail="test@test.com",
+            notifyHP="01022223333",
+            paymentName="홍길동",
+            settleCost="10000"
+        )
+        result = self.baseService.paymentRequest(CorpNum, paymentForm)
+
+        print(result)
+
+    def test_paymentRequest_with_UserID(self):
+
+        CorpNum = "1234567890"
+        UserID = "testkorea"
+        paymentForm = PaymentForm(
+            settlerName="담당자명",
+            settlerEmail="test@test.com",
+            notifyHP="01022223333",
+            paymentName="홍길동",
+            settleCost="10000"
+        )
+        result = self.baseService.paymentRequest(CorpNum, paymentForm, UserID)
+
+        print(result)
+
+    def test_GetSettleResult(self):
+        test_settle_code = "202303070000000052"
+        result = self.baseService.getSettleResult(
+            self.testCorpNum, test_settle_code)
+        print(result)
+
+    def test_GetUseHistory(self):
+        SDate = "20230101"
+        EDate = "20230102"
+        result = self.baseService.getUseHistory(self.testCorpNum, SDate, EDate)
+        print(result)
+
+    def test_GetPaymentHistory(self):
+        SDate = "20230101"
+        EDate = "20230102"
+        result = self.baseService.getPaymentHistory(
+            self.testCorpNum, SDate, EDate)
+        print(result)
+
+    def test_Refund(self):
+        refundForm = RefundForm(
+            contactname="환불신청테스트",
+            tel="01077777777",
+            requestpoint="10",
+            accountbank="국민",
+            accountnum="123123123-123",
+            accountname="예금주",
+            reason="",
+        )
+        result = self.baseService.refund(
+            self.testCorpNum, refundForm, self.testUserID)
+        print(result)
+
+    def test_RefundHistory(self):
+        result = self.baseService.getRefundHistory(self.testCorpNum)
+        print(result)
+
+    def test_getBalance(self):
+        result = self.baseService.getBalance(self.testCorpNum)
+        print(result)
+
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(BaseTest)
+    unittest.TextTestRunner(verbosity=2).run(suite)
