5ec091f35f858832cbc476ef7992ff84b7095cc5
AlBan93 <albaneo0724@gmail.com>
2023-08-11 10:50:21 +0900

    AttachFile 추가로 인한 버전 업데이트

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 import sys
 
-version = "1.60.1"
+version = "1.60.2"
 
 if sys.version_info <= (2, 5):
     error = "ERROR: popbill requires Python Version 2.6 or above...exiting."
