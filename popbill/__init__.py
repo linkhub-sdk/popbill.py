269748af6893877583b24207cd3c1616e64535b3
john <john@linkhub.co.kr>
2025-08-04 16:04:46 +0900

    version up

--- popbill/__init__.py
+++ popbill/__init__.py
@@ -20,7 +20,7 @@ from .messageService import *
 from .statementService import *
 from .taxinvoiceService import *
 
-__version__ = "1.60.3"
+__version__ = "1.61.0"
 Version = __version__  # for backward compatibility
 __all__ = [
     "PopbillException",
